6b2ea55803f911dfb36f46db9467e1c3c3ef3285
Michael Schötz <schoetz.michael@gmail.com>
2020-11-18 17:28:36 +0100

    refactored yolo2sly converter - added multiprocessing - all images are now exported as png - fixed issue #60

--- tools/label_converters/yolo2sly/yolo2sly.py
+++ tools/label_converters/yolo2sly/yolo2sly.py
@@ -47,11 +47,23 @@ import shutil
 import cv2
 import numpy as np
 from tqdm import tqdm
+from pathlib import Path
+import multiprocessing as mp
 
 from ..helpers import fsoco_classes, class_id_to_fsoco
 from ..helpers import supervisely_template, supervisely_bbox_template
 
-SUPPORTED_IMAGE_FORMATS = ["jpg", "JPG", "jpeg", "JPEG", "png", "PNG"]
+SUPPORTED_IMAGE_FORMATS = [".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG"]
+EXPORT_IMAGE_FORMAT = ".png"
+DEBUG_DISABLE_MULTIPROCESSING = False
+
+darknet_images_folder = None
+darknet_labels_folder = None
+sly_project_folder = None
+sly_labels_folder = None
+sly_images_folder = None
+image_files = []
+darknet_label_files = []
 
 
 def min_max_clip(min_val, max_val, val):
@@ -60,13 +72,8 @@ def min_max_clip(min_val, max_val, val):
     return clipped_value
 
 
-def main(
-    images_folder=None,
-    darknet_labels_folder=None,
-    out_path=None,
-    project_name=None,
-    dataset_name=None,
-):
+def create_output_folder(out_path: str, project_name: str, dataset_name: str):
+    global sly_project_folder, sly_labels_folder, sly_images_folder
     sly_project_folder = os.path.join(out_path, project_name)
     sly_labels_folder = os.path.join(out_path, project_name, dataset_name, "ann")
     sly_images_folder = os.path.join(out_path, project_name, dataset_name, "img")
@@ -74,82 +81,95 @@ def main(
     os.makedirs(sly_labels_folder, exist_ok=True)
     os.makedirs(sly_images_folder, exist_ok=True)
 
-    filenames = []
-    for f in os.listdir(images_folder):
-        # Get file extension without leading dot '.'
-        extension = os.path.splitext(f)[1][1:]
-        if extension in SUPPORTED_IMAGE_FORMATS:
-            filenames.append(f)
 
-    darknet_labels = []
-    for f in os.listdir(darknet_labels_folder):
-        # Get all annotations files to match them with the images
-        extension = os.path.splitext(f)[1][1:]
-        if extension == "txt":
-            darknet_labels.append(f)
+def convert_label(image_file: Path):
+    label_file = None
 
-    for filename in tqdm(filenames):
-        label_file = None
-        for f in darknet_labels:
-            # Match image and label files
-            if os.path.splitext(filename)[0] == os.path.splitext(f)[0]:
-                label_file = f
-                break
+    label_file = list(darknet_labels_folder.glob(f"{image_file.stem}*.txt"))[0]
+    if label_file is None:
+        print(f"No label found for {image_file}!")
+        return
 
-        sly_label_file = os.path.join(
-            sly_labels_folder, os.path.splitext(filename)[0] + ".json"
-        )
-        sly_label = supervisely_template()
+    sly_label_file = os.path.join(
+        sly_labels_folder, os.path.splitext(image_file.stem)[0] + ".json"
+    )
+    sly_label = supervisely_template()
 
-        if label_file is not None:
-            image = cv2.imread(os.path.join(images_folder, filename))
-            image_height, image_width = image.shape[:2]
-            sly_label["size"]["height"] = image_height
-            sly_label["size"]["width"] = image_width
+    image = cv2.imread(str(image_file))
+    image_height, image_width = image.shape[:2]
+    sly_label["size"]["height"] = image_height
+    sly_label["size"]["width"] = image_width
 
-            with open(os.path.join(darknet_labels_folder, label_file), "r") as f:
-                labels = [line.rstrip("\n").split(" ") for line in f.readlines()]
+    with open(str(label_file), "r") as f:
+        labels = [line.rstrip("\n").split(" ") for line in f.readlines()]
 
-            for label in labels:
-                class_id, box_points = int(label[0]), [float(x) for x in label[1:]]
+    for label in labels:
+        class_id, box_points = int(label[0]), [float(x) for x in label[1:]]
 
-                class_name = class_id_to_fsoco(class_id)
-                if class_name not in fsoco_classes():
-                    print(f'\033[91mSkipped not supported class: "{class_name}"\033[0m')
-                    continue
+        class_name = class_id_to_fsoco(class_id)
+        if class_name not in fsoco_classes():
+            print(f'\033[91mSkipped not supported class: "{class_name}"\033[0m')
+            continue
 
-                pixel_bb_x = int(np.round(box_points[0] * image_width))
-                pixel_bb_y = int(np.round(box_points[1] * image_height))
-                pixel_bb_w = int(np.round(box_points[2] * image_width))
-                pixel_bb_h = int(np.round(box_points[3] * image_height))
+        pixel_bb_x = int(np.round(box_points[0] * image_width))
+        pixel_bb_y = int(np.round(box_points[1] * image_height))
+        pixel_bb_w = int(np.round(box_points[2] * image_width))
+        pixel_bb_h = int(np.round(box_points[3] * image_height))
 
-                x1, x2 = (pixel_bb_x + pixel_bb_w / 2), (pixel_bb_x - pixel_bb_w / 2)
-                y1, y2 = (pixel_bb_y - pixel_bb_h / 2), (pixel_bb_y + pixel_bb_h / 2)
+        x1, x2 = (pixel_bb_x + pixel_bb_w / 2), (pixel_bb_x - pixel_bb_w / 2)
+        y1, y2 = (pixel_bb_y - pixel_bb_h / 2), (pixel_bb_y + pixel_bb_h / 2)
 
-                # Clip pixel values to valid image space
-                upper_left = [
-                    min_max_clip(0, image_width, x1),
-                    min_max_clip(0, image_height, y1),
-                ]
-                lower_right = [
-                    min_max_clip(0, image_width, x2),
-                    min_max_clip(0, image_height, y2),
-                ]
+        # Clip pixel values to valid image space
+        upper_left = [
+            min_max_clip(0, image_width, x1),
+            min_max_clip(0, image_height, y1),
+        ]
+        lower_right = [
+            min_max_clip(0, image_width, x2),
+            min_max_clip(0, image_height, y2),
+        ]
 
-                sly_bbox = supervisely_bbox_template()
-                sly_bbox["classTitle"] = class_name
-                sly_bbox["points"]["exterior"] = [upper_left, lower_right]
-                sly_label["objects"].append(sly_bbox)
+        sly_bbox = supervisely_bbox_template()
+        sly_bbox["classTitle"] = class_name
+        sly_bbox["points"]["exterior"] = [upper_left, lower_right]
+        sly_label["objects"].append(sly_bbox)
 
-        # Save labels file
-        with open(sly_label_file, "w") as f:
-            json.dump(sly_label, f, indent=4)
+    # Save labels file
+    with open(sly_label_file, "w") as f:
+        json.dump(sly_label, f, indent=4)
 
-        # Save image file
-        shutil.copy(
-            os.path.join(images_folder, filename),
-            os.path.join(sly_images_folder, filename),
-        )
+    # Save image file
+    cv2.imwrite(f"{sly_images_folder}/{image_file.name}{EXPORT_IMAGE_FORMAT}", image)
+
+
+def main(
+    images_folder=None,
+    labels_folder=None,
+    out_path=None,
+    project_name=None,
+    dataset_name=None,
+):
+    global darknet_images_folder, darknet_labels_folder, image_files, darknet_label_files
+    darknet_images_folder = Path(images_folder)
+    darknet_labels_folder = Path(labels_folder)
+    create_output_folder(out_path, project_name, dataset_name)
+
+    image_files = [
+        file
+        for file in darknet_images_folder.glob("*")
+        if (file.suffix in SUPPORTED_IMAGE_FORMATS)
+    ]
+    darknet_label_files = [file for file in darknet_labels_folder.glob("*.txt")]
+
+    if DEBUG_DISABLE_MULTIPROCESSING:
+        for image_file in tqdm(image_files):
+            convert_label(image_file)
+
+    else:
+        with tqdm(total=len(image_files)) as pbar:
+            with mp.Pool() as pool:
+                for res in pool.imap(convert_label, image_files):
+                    pbar.update(1)
 
     # Use FSOCO project meta file
     shutil.copy(

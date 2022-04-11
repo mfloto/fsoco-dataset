54f87d2ee7c790ded32f20feba164b22741ff4c6
David Dodel <david.dodel@hotmail.com>
2022-04-11 10:15:58 +0200

    Fix Website Checks CI (#239)
    * Simplify test via response text without rendering
    
    * Correctly use response text
    
    * Add 'what' param to request
    
    * Update GAPPS_URL

--- docs/_tests/test_contribution_procedure_status_page.py
+++ docs/_tests/test_contribution_procedure_status_page.py
@@ -5,7 +5,7 @@ import requests
 from requests_html import HTMLSession
 from typing import List
 
-GAPPS_URL = "https://script.google.com/macros/s/AKfycbwe9WgdWy_nsfyk1zC13pGc-ZnoJ4iRGvvJyIXZ2h4buI5MWLTL/exec"
+GAPPS_URL = "https://script.google.com/macros/s/AKfycbzxi0VKZJPCpySqvnxiGLsfBYOiHuxKo2Wtg4dONoxI_Huw-YkjqJVmBGCfGS7CfhPJ/exec"
 SKIP_DESC_FLAG = "skip_status_page_test"
 
 def get_teams() -> List[str]:
@@ -56,18 +56,12 @@ def get_teams() -> List[str]:
 def test_google_app_script_response():
     session = HTMLSession()
     teams = get_teams()
-    responses = []
+    response_status = []
     for team in teams:
         logging.info(f"Getting page for {team}")
-        r = session.get(GAPPS_URL, params={"team_name": team})
-        r.html.render(sleep=4, timeout=15.0)
-        responses.append(r.html.text)
-    # render for some reason doesn't execute the GApp JS script correctly
-    # The User HTML generated can be searched through regex though
-    # "Success" => team_name found; "Fail" => team_name not found
-    response_status = [
-        True if response.find("Success") >= 0 else False for response in responses
-    ]
+        r = session.get(GAPPS_URL, params={"team_name": team, "what": "get_job_status"})
+        status = True if r.text.find(team) >= 0 else False
+        response_status.append(status)
     assert all(response_status), [
         team_name for team_name, res in zip(teams, response_status) if not res
     ]
